5eac4bd713b3bbaf6d8e50e5a23b3f22ec66be6e
adnope <anhduyxh7@gmail.com>
2024-09-02 23:32:25 +0700

    Added my scripts specifically for my use cases

---
+++ MyScripts/MyScripts/cons_mode_control.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Battery conservation mode control"
+echo "[1] Enable"
+echo "[2] Disable"
+
+echo -n "Enter your choice: "
+read confirm
+
+while [[ "$confirm" != "1" ]] && [[ "$confirm" != "2" ]]
+do
+	echo "That's not a choice wtf?"
+	read confirm
+done
+
+if [ "$confirm" == "1" ]; then
+	echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode > /dev/null
+	echo "Conservation Mode: on"
+	
+elif [ "$confirm" == "2" ]; then
+	echo 0 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode > /dev/null
+	echo "Conservation Mode: off"
+fi
---
+++ MyScripts/MyScripts/move_cursor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import pyautogui
+import time
+
+while(True):
+    pyautogui.moveRel(0, 1, 0)
+    time.sleep(300)
