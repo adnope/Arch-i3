3747337f56250ac2a57e4715044335a570796d32
adnope <anhduyxh7@gmail.com>
2025-04-30 02:44:36 +0700

    Updated i3 keybinds

--- polybar/.config/polybar/popup-calendar.sh
+++ polybar/.config/polybar/popup-calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BAR_HEIGHT=20  # polybar height
+BAR_HEIGHT=12  # polybar height
 BORDER_SIZE=2  # border size from your wm settings
 YAD_WIDTH=250  # 222 is minimum possible value
 YAD_HEIGHT=200 # 193 is minimum possible value
@@ -15,24 +15,35 @@ case "$1" in
     eval "$(xdotool getmouselocation --shell)"
     eval "$(xdotool getdisplaygeometry --shell)"
 
-    # X
-    if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
-        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
-    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
-        : $((pos_x = BORDER_SIZE))
-    else #Center
-        : $((pos_x = X - YAD_WIDTH / 2))
-    fi
+    # # X
+    # if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
+    #     : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
+    # elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
+    #     : $((pos_x = BORDER_SIZE))
+    # else #Center
+    #     : $((pos_x = X - YAD_WIDTH / 2))
+    # fi
 
-    # Y
-    if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
+    # # Y
+    # if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
+    #     : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
+    # else #Top
+    #     : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
+    # fi
+
+     # Center horizontally
+    : $((pos_x = (WIDTH - YAD_WIDTH) / 2))
+
+    # Y position based on mouse Y
+    if [ "$Y" -gt "$((HEIGHT / 2))" ]; then # Bottom half of screen
         : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
-    else #Top
+    else # Top half of screen
         : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
     fi
 
-    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+    yad --calendar --undecorated --close-on-unfocus --fixed --no-buttons \
         --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
+        --fontname="JetbrainsMono Nerd Font 14" \
         --title="yad-calendar" --borders=0 >/dev/null &
     ;;
 *)
